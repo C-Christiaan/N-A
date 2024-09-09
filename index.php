53e4244ee05f7a87cf7452e6532530a14197a708
Jayven <jayvenaltenberg@gmail.com>
2024-09-09 22:54:01 +0200

    added some comments and echo the grid system

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 require 'obj.php';
 $game = new GameOfLife(5, 3);
+$game->printGrid();
--- obj.php
+++ obj.php
@@ -1,14 +1,17 @@
 <?php
 class GameOfLife{
+    //names the variables to later use
     private $width;
     private $height;
     private $grid;
+
+    // when you call the function game of life  __construct is instatntly called so you dont need to
     public function __construct($width, $height){
         $this->width = $width;
         $this->height = $height;
         $this->grid = array();
         for ($i = 0; $i < $height; $i++) { 
-            $this->grid[$i] = array(); //this creates a new array (inside the array i hope) with the width
+            $this->grid[$i] = array(); //this creates a new array with the width
             for ($j = 0; $j < $width; $j++) {
                 if (rand(1, 10) == 5) {
                     $Cell_Status = "A"; //cell is alive
@@ -16,6 +19,14 @@ class GameOfLife{
                     $Cell_Status = "D"; //cell is dead
                 }
                 $this->grid[$i][$j] = $Cell_Status; //this inserts a value into the previously made array
+            }
+            echo PHP_EOL;
+        }
+    }
+    public function PrintGrid() {
+        foreach ($this->grid as $row) {
+            foreach ($row as $cell) {
+                echo $cell . ' '; // Print each cell with a space for readability
             }
             echo PHP_EOL;
         }
