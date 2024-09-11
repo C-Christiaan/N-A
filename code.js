254f0614a83040de5c92a2d895f551e6376af5b9
Siilvan <silvan.groenveld@gmail.com>
2024-09-11 15:08:35 +0200

    stash

--- code.js
+++ code.js
@@ -205,7 +205,7 @@ function button_submit() {
         alert("Error: cell width must be between 3 and 25");
     else {
         cell_x_count = cx;
-        cell_y_count = cy;
+        cell_y_count = cy;          	q
         cell_width = cd;
         button_clear();
 
